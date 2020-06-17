7c7423fb300f55dc9b624b320d190e8635134796
GitM423 <matthias.p.hoeh@gmail.com>
2020-06-17 14:36:02 +0200

    v0.0.1

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+let firstName = document.getElementById('first-name')
+let lastName = document.getElementById('last-name')
+let email = document.getElementById('email')
+let thema = document.getElementById('thema')
+let nachricht = document.getElementById('nachricht')
+
+function submitIt() {
+    console.log("working")
+    console.log(firstName.value)
+    console.log(lastName.value)
+    console.log(email.value)
+    console.log(thema.value)
+    console.log(nachricht.value)
+}
